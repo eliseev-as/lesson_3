1102745b888f416abb0567eaeb42698380a9dee6
a.eliseev <a.eliseev@equiron.com>
2024-08-12 08:59:35 +0300

    Lesson 3

---
+++ exercise_2.py
@@ -0,0 +1,16 @@
+# https://ru.wikipedia.org/wiki/%D0%A5%D1%80%D0%BE%D0%BD%D0%BE%D0%BB%D0%BE%D0%B3%D0%B8%D1%8F_%D1%8D%D0%B2%D0%BE%D0%BB%D1%8E%D1%86%D0%B8%D0%B8_%D1%87%D0%B5%D0%BB%D0%BE%D0%B2%D0%B5%D0%BA%D0%B0
+stage_1 = input("1 этап развития человека: ")
+stage_2 = input("2 этап развития человека: ")
+stage_3 = input("3 этап развития человека: ")
+stage_4 = input("4 этап развития человека: ")
+stage_5 = input("5 этап развития человека: ")
+stage_6 = input("6 этап развития человека: ")
+stage_7 = input("7 этап развития человека: ")
+stage_8 = input("8 этап развития человека: ")
+stage_9 = input("9 этап развития человека: ")
+stage_10 = input("10 этап развития человека: ")
+stage_11 = input("11 этап развития человека: ")
+stage_12 = input("12 этап развития человека: ")
+stage_13 = input("13 этап развития человека: ")
+print(stage_1, stage_2, stage_3, stage_4, stage_5, stage_6, stage_7, stage_8, stage_9, stage_10, stage_11, stage_12,
+      stage_13, sep="=>")
